5e0529000f4eac8d77560b37208341ddb7384a97
DG <dgeoffri@gmail.com>
2018-12-21 00:31:52 -0600

    adding day13a

--- day13a.c
+++ day13a.c
@@ -2,9 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct Coord {
+	int x;
+	int y;
+};
+
 struct Car {
 	int x, y, dir, nextTurn;     // Dir:  0=up  1=right  2=down  3=left   NextTurn:  0=left  1=straight  2=right
-	struct Car * nextCar;	
+	struct Car * nextCar;
+	int alreadyMoved;
 };
 
 struct Car * newCar(int dir, int nextTurn, struct Car * oldCar) {
@@ -16,193 +22,208 @@ struct Car * newCar(int dir, int nextTurn, struct Car * oldCar) {
 	return newcar;
 }
 
-void marchCars(struct Car * carsHead, char grid[150][151]) {
-	struct Car * car = carsHead;
-	while (car != NULL) {
-		switch (car->dir) {
-			case 0:
-				car->y--;
-				break;
-			case 1:
-				car->x++;
-				break;
-			case 2:
-				car->y++;
-				break;
-			case 3:
-				car->x--;
-				break;
-		}
-		switch (grid[car->y][car->x]) {                         // Dir:  0=up  1=right  2=down  3=left   NextTurn:  0=left  1=straight  2=right
-			case '/':
+struct Coord * marchCars(struct Car * cargrid[150][151], char grid[150][151]) {
+	struct Car * car;
+	struct Coord * collision;
+	int x, y;
+	for (y=0; y<150; y++)
+		for (x=0; x<150; x++)
+			if (cargrid[y][x])
+				cargrid[y][x]->alreadyMoved = 0;
+	for (y=0; y<150; y++)
+		for (x=0; x<150; x++) {
+			if ((cargrid[y][x]) && !(cargrid[y][x]->alreadyMoved)) {
+				car = cargrid[y][x];
+				int xnew = x, ynew = y;
 				switch (car->dir) {
 					case 0:
-						car->dir = 1;
+						ynew--;
 						break;
 					case 1:
-						car->dir = 0;
+						xnew++;
 						break;
 					case 2:
-						car->dir = 3;
+						ynew++;
 						break;
 					case 3:
-						car->dir = 2;
+						xnew--;
 						break;
 				}
-				break;
-			case '\\':
-				switch (car->dir) {
-					case 0:
-						car->dir = 3;
-						break;
-					case 1:
-						car->dir = 2;
-						break;
-					case 2:
-						car->dir = 1;
-						break;
-					case 3:
-						car->dir = 0;
-						break;
+				if (cargrid[ynew][xnew]) {
+					collision = malloc(sizeof(struct Coord));
+					collision->x = xnew;
+					collision->y = ynew;
+					return collision;
+				} else {
+					cargrid[ynew][xnew] = cargrid[y][x];
+					cargrid[y][x] = NULL;
 				}
-				break;
-			case '+':                         // Dir:  0=up  1=right  2=down  3=left   NextTurn:  0=left  1=straight  2=right
-				switch (car->nextTurn) {
-					case 0:
+				switch (grid[ynew][xnew]) {                // Dir:  0=up  1=right  2=down  3=left   NextTurn:  0=left  1=straight  2=right
+					case '/':
 						switch (car->dir) {
 							case 0:
-								car->dir = 3;
+								car->dir = 1;
 								break;
 							case 1:
 								car->dir = 0;
 								break;
 							case 2:
-								car->dir = 1;
+								car->dir = 3;
 								break;
 							case 3:
 								car->dir = 2;
 								break;
 						}
 						break;
-					case 2:
+					case '\\':
 						switch (car->dir) {
 							case 0:
-								car->dir = 1;
+								car->dir = 3;
 								break;
 							case 1:
 								car->dir = 2;
 								break;
 							case 2:
-								car->dir = 3;
+								car->dir = 1;
 								break;
 							case 3:
 								car->dir = 0;
 								break;
 						}
 						break;
-					break;
+					case '+':                         // Dir:  0=up  1=right  2=down  3=left   NextTurn:  0=left  1=straight  2=right
+						switch (car->nextTurn) {
+							case 0:
+								switch (car->dir) {
+									case 0:
+										car->dir = 3;
+										break;
+									case 1:
+										car->dir = 0;
+										break;
+									case 2:
+										car->dir = 1;
+										break;
+									case 3:
+										car->dir = 2;
+										break;
+								}
+								break;
+							case 2:
+								switch (car->dir) {
+									case 0:
+										car->dir = 1;
+										break;
+									case 1:
+										car->dir = 2;
+										break;
+									case 2:
+										car->dir = 3;
+										break;
+									case 3:
+										car->dir = 0;
+										break;
+								}
+								break;
+							break;
+						}
+						car->nextTurn++;
+						car->nextTurn %= 3;
+						break;
 				}
-				car->nextTurn++;
-				car->nextTurn %= 3;
-				break;
+				car->alreadyMoved = 1;
+			}
 		}
-		car = car->nextCar;
-	}
+	return NULL;
 }
 
-void showCars(struct Car * carsHead) {
+void showCars(struct Car * cargrid[150][151]) {
 	struct Car * car;
-	int i;
-	car = carsHead;
-	i = 0;
-	while (car != NULL) {
-		switch (car->dir) {
-			case 0:
-				printf ("Car %d has location (%d, %d) and faces up\n", i++, car->x, car->y);
-				break;
-			case 1:
-				printf ("Car %d has location (%d, %d) and faces right\n", i++, car->x, car->y);
-				break;
-			case 2:
-				printf ("Car %d has location (%d, %d) and faces down\n", i++, car->x, car->y);
-				break;
-			case 3:
-				printf ("Car %d has location (%d, %d) and faces left\n", i++, car->x, car->y);
-				break;
+	int x, y;
+	int i = 0;
+	for (y=0; y<150; y++)
+		for (x=0; x<150; x++) {
+			car = cargrid[y][x];
+			if (car) {
+				switch (car->dir) {
+					case 0:
+						printf ("Car %d has location (%d, %d) and faces up\n", i++, x, y);
+						break;
+					case 1:
+						printf ("Car %d has location (%d, %d) and faces right\n", i++, x, y);
+						break;
+					case 2:
+						printf ("Car %d has location (%d, %d) and faces down\n", i++, x, y);
+						break;
+					case 3:
+						printf ("Car %d has location (%d, %d) and faces left\n", i++, x, y);
+						break;
+				}
+			}
 		}
-		car = car->nextCar;
-	}
 	printf ("\n");
 }
 
-struct Car * plotCars(struct Car * carsHead, char grid[150][151]) {
+void plotCars(struct Car * cargrid[150][151], char grid[150][151]) {
 	struct Car * car;
-	struct Car * crashedCar = NULL;
-	car = carsHead;
-	while ((car != NULL) && (crashedCar == NULL)) {
-		switch (grid[car->y][car->x]) {
-			case '^':
-			case '>':
-			case 'v':
-			case '<':
-				crashedCar = car;
-				grid[car->y][car->x] = 'X';
-				break;
-				
-			default:
+	int x, y;
+	for (y=0; y<150; y++)
+		for (x=0; x<150; x++) {
+			car = cargrid[y][x];
+			if (car) {
 				switch (car->dir) {
 					case 0:
-						grid[car->y][car->x] = '^';
+						grid[y][x] = '^';
 						break;
 					case 1:
-						grid[car->y][car->x] = '>';
+						grid[y][x] = '>';
 						break;
 					case 2:
-						grid[car->y][car->x] = 'v';
+						grid[y][x] = 'v';
 						break;
 					case 3:
-						grid[car->y][car->x] = '<';
+						grid[y][x] = '<';
 						break;
 				}
-				break;
+			}
 		}
-		car = car->nextCar;
-	}
-	return (crashedCar);
 }
 
 int main(int argc, char **argv) {
-	FILE	*infile;
-	char	grid[150][151];
-	struct Car * cargrid[150][151];
-	char	destgrid[150][151];
-	struct Car * car = NULL;
-	struct Car * crashedCar = NULL;
-	int	x, y, i;
+	FILE		*infile;
+	FILE		*outfile;
+	char		outfilename[64];
+	struct Car * 	cargrid[150][151];
+	char		grid[150][151];
+	char		destgrid[150][151];
+	struct Car * 	car = NULL;
+	struct Coord * 	collisionPoint;
+	int		x, y, i = 0;
 
 	infile = fopen("day13.txt", "r");
 	fread (grid, 150, 151, infile);
+	fclose (infile);
 
 	for (y=0; y<150; y++) {
 		for (x=0; x<150; x++) {
 			switch (grid[y][x]) {
 				case '^':
 					grid[y][x] = '|';
-					cargrid[y][x] = nweCar(0, 0, cargrid[y][x]);
+					cargrid[y][x] = newCar(0, 0, cargrid[y][x]);
 					break;
 				case '>':
 					grid[y][x] = '-';
-					cargrid[y][x] = nweCar(1, 0, cargrid[y][x]);
+					cargrid[y][x] = newCar(1, 0, cargrid[y][x]);
 					break;
 				case 'v':
 					grid[y][x] = '|';
-					cargrid[y][x] = nweCar(2, 0, cargrid[y][x]);
+					cargrid[y][x] = newCar(2, 0, cargrid[y][x]);
 					break;
 				case '<':
 					grid[y][x] = '-';
-					cargrid[y][x] = nweCar(3, 0, cargrid[y][x]);
+					cargrid[y][x] = newCar(3, 0, cargrid[y][x]);
 					break;
-				case default:
+				default:
 					cargrid[y][x] = NULL;
 					break;
 			}
@@ -210,22 +231,21 @@ int main(int argc, char **argv) {
 		grid[y][150] = 0;
 	}
 
-	memcpy (destgrid, grid, sizeof(grid));
-	while ((crashedCar=plotCars(carsHead, destgrid)) == NULL) {
-		marchCars(carsHead, grid);
+	// memcpy (destgrid, grid, sizeof(grid));
+	// while ((crashedCar=plotCars(carsHead, destgrid)) == NULL) {
+	//	marchCars(carsHead, grid);
+	//	memcpy (destgrid, grid, sizeof(grid));
+	// }
+	while (!(collisionPoint = marchCars(cargrid, grid))) {
+		// showCars(cargrid);
 		memcpy (destgrid, grid, sizeof(grid));
+		plotCars(cargrid, destgrid);
+		snprintf(outfilename, sizeof(outfilename), "day13a-frame%08d.txt", i++);
+		outfile = fopen(outfilename, "w");
+		for (y=0; y<150; y++)
+			fprintf (outfile, "%s\n", destgrid[y]);
+		fclose (outfile);
+		showCars(cargrid);
 	}
-
-	printf("Crash!  Two carts collided at %d,%d\n", crashedCar->x, crashedCar->y);
-	showCars(carsHead);
-	plotCars(carsHead, destgrid);
-	for (y=0; y<150; y++)
-		printf ("%s\n", destgrid[y]);
-
-	car = carsHead;
-	while (car != NULL) {
-		struct Car * oldcar = car;
-		car = car->nextCar;
-		free(oldcar);
-	}
+	printf ("There was a crash!  Carts collided at %d,%d\n", collisionPoint->x, collisionPoint->y);
 }
