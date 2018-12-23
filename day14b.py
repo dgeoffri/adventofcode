b60f3bb207d14eac80fc2b1043962796f0420a40
DG <dgeoffri@gmail.com>
2018-12-22 23:08:06 -0600

    updating day14b.py

--- day14b.py
+++ day14b.py
@@ -14,10 +14,16 @@ recipes = [3, 7]
 elf1 = 0
 elf2 = 1
 num = [int(digit) for digit in sys.argv[1]]
+oldlen = 0
+matched = False
 
-print num, recipes
-while (recipes[-len(num):] != num):
+while not matched:
+    oldlen = len(recipes)
     elf1, elf2 = iterate(recipes, elf1, elf2)
-    print num, recipes
+    # print recipes, oldlen, len(recipes)
+    for i in xrange(len(recipes)-oldlen):
+        if (recipes[-len(num)-i:len(recipes)-i] == num):
+            matched = True
+            print "matched after", len(recipes)-len(num)-i, "recipes"
+            break
 
-print "matched after", len(recipes)-len(num), "recipes"
