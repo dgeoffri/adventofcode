d58636f4aa05efc03791e5100c978ee21cb9da1f
DG <dgeoffri@gmail.com>
2019-05-14 23:23:52 -0500

    Day9b

---
+++ 2015/day9b.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import re, itertools
+
+with open('day9.txt', 'r') as inputfile:
+	inputdata = inputfile.read().splitlines()
+	regex = re.compile(r'(.+) to (.+) = (\d+)')
+	cities = set()
+	distances=dict()
+	for line in inputdata:
+		data = regex.match(line).groups()
+		cities.add(data[0])
+		cities.add(data[1])
+		distances[(data[0], data[1])] = int(data[2])
+		distances[(data[1], data[0])] = int(data[2])
+	results=list()
+	for i in itertools.permutations(cities):
+		distance = 0
+		for j in xrange(len(i)-1):
+			distance += distances[(i[j], i[j+1])]
+		# print "{} - {} miles".format(', '.join(i), distance)
+		results.append([distance, i])
+        results.sort()
+	print "The shortest path is {} miles long".format(results[0][0])
+        print "The longest path is {} miles long".format(results[-1][0])
---
+++ 2015/day9b_sqlite3.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import re, itertools, sqlite3
+
+with open('day9.txt', 'r') as inputfile:
+	inputdata = inputfile.read().splitlines()
+	cities = set([i.split(' ')[0] for i in inputdata] + [i.split(' ')[2] for i in inputdata])
+	regex = re.compile(r'(.+) to (.+) = (\d+)')
+	sqldb = sqlite3.connect(":memory:")
+	sqldb.execute("CREATE TABLE distances (city1 VARCHAR, city2 VARCHAR, distance INT);")
+	data = [[f(x) for f, x in zip((str, str, int), regex.match(line).groups())] for line in inputdata]
+	for i in data:
+		sqldb.execute("INSERT INTO distances (city1, city2, distance) VALUES (\"{}\", \"{}\", {});".format(i[0], i[1], i[2]))
+		sqldb.execute("INSERT INTO distances (city1, city2, distance) VALUES (\"{}\", \"{}\", {});".format(i[1], i[0], i[2]))
+	sqldb.commit()
+	results=list()
+	for i in itertools.permutations(cities):
+		distance = 0
+		for j in xrange(len(i)-1):
+			distance += sqldb.execute("SELECT distance FROM distances WHERE city1 LIKE '{}' AND city2 LIKE '{}';".format(i[j], i[j+1])).fetchall()[0][0]
+		# print "{} - {} miles".format(', '.join(i), distance)
+		results.append([distance, i])
+        results.sort()
+	print "The shortest path is {} miles long".format(results[0][0])
+        print "The longest path is {} miles long".format(results[-1][0])
