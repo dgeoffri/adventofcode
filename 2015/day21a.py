4e446da42a0fe0ea2e118e92fe2ba4be903916fb
David Geoffrion <dgeoffri@suzaku.attlocal.net>
2019-06-14 10:15:42 -0500

    boilerplate/constant definition start on day 21 (2015)

---
+++ 2015/day21a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+ITEMS = { 'Dagger': 	{'Cost': 8,   'Damage': 4, 'Armor': 0},
+		  'Shortsword': {'Cost': 10,  'Damage': 5, 'Armor': 0},
+		  'Warhammer': 	{'Cost': 25,  'Damage': 6, 'Armor': 0},
+		  'Longsword': 	{'Cost': 40,  'Damage': 7, 'Armor': 0},
+		  'Greataxe': 	{'Cost': 74,  'Damage': 8, 'Armor': 0},
+		  'Leather': 	{'Cost': 13,  'Damage': 0, 'Armor': 1},
+		  'Chainmail': 	{'Cost': 31,  'Damage': 0, 'Armor': 2},
+		  'Splintmail': {'Cost': 53,  'Damage': 0, 'Armor': 3},
+		  'Bandedmail': {'Cost': 75,  'Damage': 0, 'Armor': 4},
+		  'Platemail': 	{'Cost': 102, 'Damage': 0, 'Armor': 5},
+		  'No Armor': 	{'Cost': 0,   'Damage': 0, 'Armor': 0},
+		  'Damage +1': 	{'Cost': 25,  'Damage': 1, 'Armor': 0},
+		  'Damage +2': 	{'Cost': 50,  'Damage': 2, 'Armor': 0},
+		  'Damage +3': 	{'Cost': 100, 'Damage': 3, 'Armor': 0},
+		  'Defense +1': {'Cost': 20,  'Damage': 0, 'Armor': 1},
+		  'Defense +2': {'Cost': 40,  'Damage': 0, 'Armor': 2},
+		  'Defense +3': {'Cost': 80,  'Damage': 0, 'Armor': 3},
+		  'Karate 1':   {'Cost': 0,   'Damage': 0, 'Armor': 0},
+		  'Karate 2':   {'Cost': 0,   'Damage': 0, 'Armor': 0} }
