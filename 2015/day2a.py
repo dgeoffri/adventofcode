1f56ed5b459e7d6a9837dcc3eaf74b21388dc814
David Geoffrion <dgeoffri@suzaku.attlocal.net>
2019-06-22 09:26:11 -0500

    update 2015day2 both parts to accept an optional input filename

--- 2015/day2a.py
+++ 2015/day2a.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
+import sys
+
 total_area = 0
-with open('day2.txt', 'r') as infile:
+with open(sys.argv[1] if len(sys.argv) > 1 else 'day2.txt', 'r') as infile:
 	for box in infile:
 		l,w,h=map(int,box.rstrip().split('x'))
 		total_area += 2*l*w+2*w*h+2*h*l+min(l*w,w*h,h*l)
--- 2015/day2b.py
+++ 2015/day2b.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
+import sys
+
 total_area = 0
-with open('day2.txt', 'r') as infile:
+with open(sys.argv[1] if len(sys.argv) > 1 else 'day2.txt', 'r') as infile:
 	for box in infile:
 		l,w,h=map(int,box.rstrip().split('x'))
 		total_area += 2*min(l+w,w+h,h+l)+l*w*h
