4b5ea8d19156420f29df4ca993ede4ec58145f61
David Geoffrion <dgeoffri@gmail.com>
2019-06-25 19:29:26 -0500

    updated 2015day3 python implementations to take a filename for input in the standard way, added go implementation of 2015day3a

---
+++ 2015/day3a.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"io/ioutil"
+)
+
+func printusage() {
+	fmt.Printf("Usage: %s <directions file>\n\nFind how many unique addresses Santa visits based on directions given in <directions file>\n", os.Args[0])
+	log.Fatal("FATAL: no filename given")
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+type Address struct {
+	x, y int
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		printusage()
+	}
+
+	dat, err := ioutil.ReadFile(os.Args[1])
+	check(err)
+
+	unique_addresses := make(map[Address]bool)
+	addresses := make([]Address, len(dat)+1)
+	current_address := Address{0,0}
+	unique_addresses[current_address] = true
+
+	for i := 0; i < len(dat); i++ {
+		addresses[i] = current_address
+		switch(dat[i]) {
+			case '^':
+				current_address.y++
+			case 'v':
+				current_address.y--
+			case '<':
+				current_address.x--
+			case '>':
+				current_address.x++
+		}
+		unique_addresses[current_address] = true
+	}
+	addresses[len(dat)] = current_address
+
+	fmt.Printf ("%v houses are visted at least once\n", len(unique_addresses))
+
+	for i := 0; i < len(addresses); i++ {
+		fmt.Println(addresses[i])
+	}
+}
