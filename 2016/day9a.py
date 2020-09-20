ab7ae000c22168eaa5004898a0fb97820fdbba16
DG <dgeoffri@droptwo.david.geoffrions.com>
2020-09-20 04:15:32 -0400

    day9 both parts added

---
+++ 2016/day9a.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+def decompress(data):
+	TEXT = 0
+	GETLEN = 1	
+	GETCOUNT = 2
+	DECOMPRESS = 3
+
+	data = list(data)
+	decompressed_data = ""
+	repeated_string = ""
+	compressed_length = ""
+	compressed_count = ""
+	state = TEXT
+
+	while len(data):
+		nextchar = data.pop(0)
+		if state == TEXT:
+			if nextchar == "(":
+				state = GETLEN
+			else:
+				decompressed_data += nextchar
+		elif state == GETLEN:
+			if nextchar == "x":
+				state = GETCOUNT
+			else:
+				compressed_length += nextchar	
+		elif state == GETCOUNT:
+			if nextchar == ")":
+				state = DECOMPRESS
+			else:
+				compressed_count += nextchar
+		elif state == DECOMPRESS:
+			repeated_string += nextchar
+			for i in xrange(int(compressed_length)-1):
+				repeated_string += data.pop(0)
+			for i in xrange(int(compressed_count)):
+				decompressed_data += repeated_string
+			repeated_string = ""
+			compressed_length = ""
+			compressed_count = ""
+			state = TEXT
+	return decompressed_data
+
+if __name__ == "__main__":
+	with open("day9.txt", "r") as inputfile:
+		data = inputfile.read().rstrip()
+	decompressed_data = decompress(data)
+	print decompressed_data	
+	print "Decompressed data are %d bytes long" % len(decompressed_data)
---
+++ 2016/day9b.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import re, sys
+markersearch = re.compile(r'\(\d+x\d+\)')
+
+def contains_markers(data):
+	return True if markersearch.search(data) else False
+
+def decompress(data):
+	TEXT = 0
+	GETLEN = 1	
+	GETCOUNT = 2
+	DECOMPRESS = 3
+
+	decompressed_data_length = 0
+	repeated_string = ""
+	compressed_length = ""
+	compressed_count = ""
+	state = TEXT
+
+	if not contains_markers(data):
+		return len(data)
+
+	data = list(data)
+	while len(data):
+		nextchar = data.pop(0)
+		if state == TEXT:
+			if nextchar == "(":
+				state = GETLEN
+			else:
+				decompressed_data_length += 1
+		elif state == GETLEN:
+			if nextchar == "x":
+				state = GETCOUNT
+			else:
+				compressed_length += nextchar	
+		elif state == GETCOUNT:
+			if nextchar == ")":
+				state = DECOMPRESS
+			else:
+				compressed_count += nextchar
+		elif state == DECOMPRESS:
+			repeated_string += nextchar
+			for i in xrange(int(compressed_length)-1):
+				repeated_string += data.pop(0)
+			for i in xrange(int(compressed_count)):
+				decompressed_data_length += decompress(repeated_string)
+			repeated_string = ""
+			compressed_length = ""
+			compressed_count = ""
+			state = TEXT
+	return decompressed_data_length
+
+if __name__ == "__main__":
+	with open("day9.txt", "r") as inputfile:
+		data = inputfile.read().rstrip()
+	decompressed_data_length = decompress(data)
+	print "Decompressed data are %d bytes long" % decompressed_data_length
