6460a0e75373f32cdf8b00b43636cc5baa44c836
DG <dgeoffri@david.geoffrions.com>
2020-12-04 12:20:36 -0600

    adding working day03a

---
+++ 2019/day03a.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+def find_distance(coords):
+	return reduce(lambda x, y: x + y, map(abs, list(coords)))
+
+def main():
+	with open('day03.txt', 'r') as f:
+		wiredata = f.read().splitlines()
+	wirelocations = []
+	for wirenumber, wiremovements in enumerate(wiredata):
+		pos = [0, 0]
+		wirelocations.append([])
+		for movement in wiremovements.split(','):
+			direction, distance = movement[0], int(movement[1:])
+			if direction == 'R':
+				dirvector = (1, 0)
+			elif direction == 'L':
+				dirvector = (-1, 0)
+			elif direction == 'U':
+				dirvector = (0, -1)
+			elif direction == 'D':
+				dirvector = (0, 1)
+			for _ in xrange(distance):
+				for axis in (0, 1):
+					pos[axis] += dirvector[axis]
+				wirelocations[wirenumber].append(tuple(pos))
+	# print "Crossings found: " + repr(crossings)
+	# nearestcrossing = findnearestcrossing(crossings, origin = origin)
+	# print "Closest crossing found has a Manhattan distance of: " + str(nearestcrossing)
+	wiresets = map(set, wirelocations)
+	crossings = reduce(lambda x, y: x & y, wiresets)
+	print "Closest intersection is {} units away.".format(sorted(map(find_distance, list(crossings)))[0])
+
+if __name__ == "__main__":
+	main()
