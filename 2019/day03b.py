fdb0e74936505864a0aaf75150c5b92b06e793d1
DG <dgeoffri@david.geoffrions.com>
2020-12-04 13:55:17 -0600

    solved day03b

---
+++ 2019/day03b.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+def find_distance(coords):
+	return reduce(lambda x, y: x + y, map(abs, list(coords)))
+
+def main():
+	with open('day03.txt', 'r') as f:
+		wiredata = f.read().splitlines()
+	wirelocations = []
+	for wirenumber, wiremovements in enumerate(wiredata):
+		pos = [0, 0]
+		wirelocations.append([])
+		for movement in wiremovements.split(','):
+			direction, distance = movement[0], int(movement[1:])
+			if direction == 'R':
+				dirvector = (1, 0)
+			elif direction == 'L':
+				dirvector = (-1, 0)
+			elif direction == 'U':
+				dirvector = (0, -1)
+			elif direction == 'D':
+				dirvector = (0, 1)
+			for _ in xrange(distance):
+				for axis in (0, 1):
+					pos[axis] += dirvector[axis]
+				wirelocations[wirenumber].append(tuple(pos))
+	wiresets = map(set, wirelocations)
+	crossings = reduce(lambda x, y: x & y, wiresets)
+	print "Closest intersection is {} units away.".format(min(map(find_distance, list(crossings))))
+	combinedsteps = [wirelocations[0].index(i) + 1 + wirelocations[1].index(i) + 1 for i in list(crossings)]
+	print "Closest intersection by number of combined steps requires {} steps.".format(min(combinedsteps))
+
+if __name__ == "__main__":
+	main()
