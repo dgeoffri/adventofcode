e8a7c74f07f364e8b7f6f717963f44fecbab98ea
DG <dgeoffri@david.geoffrions.com>
2020-12-04 23:39:47 -0600

    Day 05 both parts added

---
+++ 2020/day05b.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+if __name__ == "__main__":
+    with open('day05.txt', 'r') as f:
+        inputdata = f.read().splitlines()
+
+    seat_ids = []
+    for seat in inputdata:
+        seat_y = seat[:7]
+        seat_x = seat[7:]
+        y_min = 0
+        y_max = 127
+        x_min = 0
+        x_max = 7
+        for c in seat_y:
+            if c=='F':
+                y_max -= (1 + y_max - y_min) / 2
+            elif c=='B':
+                y_min += (1 + y_max - y_min) / 2
+            else:
+                print "Coding error"
+        if y_min != y_max:
+            print "Error, y_min != y_max"
+        for c in seat_x:
+            if c=='L':
+                x_max -= (1 + x_max - x_min) / 2
+            elif c=='R':
+                x_min += (1 + x_max - x_min) / 2
+            else:
+                print "Coding error"
+        if x_min != x_max:
+            print "Error, x_min != x_max"
+        seat_id = y_min * 8 + x_min
+        # print "Seat is in row {}, column {}.  Seat ID is {}.".format(y_min, x_min, seat_id)
+        seat_ids.append(seat_id)
+
+    for i in xrange(min(seat_ids), max(seat_ids) + 1):
+        if not i in seat_ids:
+            print "Seat {} is vacant.".format(i)
+
+
+
+
