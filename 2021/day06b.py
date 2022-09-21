a2728411bce7078777132906db98bc122fa861d2
DG <dgeoffri@david.geoffrions.com>
2022-09-21 02:39:55 -0500

    Working day06b

--- 2021/day06b.py
+++ 2021/day06b.py
@@ -1,28 +1,23 @@
 #!/usr/bin/env python3
 
-class Fish(object):
-    def __init__(self, timer):
-        self.timer = timer
-    def __repr__(self):
-        return str(self.timer)
-    def age(self):
-        if self.timer == 0:
-            self.timer = 6
-            return Fish(8)
-        else:
-            self.timer -= 1
-            return None
+class FishAges(object):
+    def __init__(self, loadfishes=[]):
+        self.slots = [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+        for fish in loadfishes:
+            assert fish >= 0 and fish <= 6
+            self.slots[fish] += 1
+    def age(self, days = 1):
+        for _ in range(days):
+            temp = self.slots.pop(0)
+            self.slots[6] += temp
+            self.slots.append(temp)
+    def count(self):
+        return sum(self.slots)
 
 if __name__ == "__main__":
     with open("day06.txt", "r") as inputfile:
-        fishes = [Fish(i) for i in map(int, inputfile.read().strip().split(','))]
-    print(fishes)
-    for days in range(256):
-        newfishes = []
-        for fish in fishes:
-            newfish = fish.age()
-            if newfish:
-                newfishes.append(newfish)
-        fishes += newfishes
-        print(days)
-    print("After {} days there are {} fishes.".format(256, len(fishes)))
+        fish_ages = FishAges(map(int, inputfile.read().strip().split(',')))
+    fish_ages.age(80)
+    print("After {} days there are {} fishes.".format(80, fish_ages.count()))
+    fish_ages.age(256 - 80)
+    print("After {} days there are {} fishes.".format(256, fish_ages.count()))
