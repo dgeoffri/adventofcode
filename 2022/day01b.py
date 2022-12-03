39f19c14ef0ddbd26657983c5a8ad068d8d57f53
DG <dgeoffri@david.geoffrions.com>
2022-12-03 12:47:57 -0600

    Day01a was wrongly named and also here is part b

---
+++ 2022/day01b.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+    with open("day01.txt", "r") as f:
+        data = f.read().splitlines()
+    calories_per_elf = []
+    running_total = 0
+    for line in data:
+        if not len(line):
+            calories_per_elf.append(running_total)
+            running_total = 0
+        else:
+            running_total += int(line)
+    print(f"The elf with the most calories has {sorted(calories_per_elf)[-1]} calories.")
+    print(f"The sum of the calories carried by the top 3 elves is {sum(sorted(calories_per_elf)[-3:])} calories.")
+
