8fe79486b13c679c1bb2034b1828502a2e5882b5
DG <dgeoffri@david.geoffrions.com>
2022-12-06 02:20:58 -0600

    Day 6 both parts ho hom i mean freaking ho freaking hum

---
+++ 2022/day06a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+def notUnique(buffer):
+    assert len(buffer) == 4, "Without being given a 4 byte string, I can't help you"
+    found = ""
+    for i in buffer:
+        if i in found:
+            return True
+        found += i
+    return False
+
+def find_start(data):
+    endbyte = 4
+    buffer = data[endbyte - 4:endbyte]
+    while notUnique(buffer):
+        endbyte += 1
+        buffer = data[endbyte - 4:endbyte]
+    return endbyte
+
+if __name__ == "__main__":
+    with open("day06.txt", "r") as f:
+        data = f.read().strip()
+    start = find_start(data)
+    print(f"The first start packet occurs after reading byte {start}.")
