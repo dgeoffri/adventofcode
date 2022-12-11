309947aaabb587a554e183562747e3f832d7c955
DG <dgeoffri@david.geoffrions.com>
2022-12-11 02:09:11 -0600

    Day 10, both parts

---
+++ 2022/day10b.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+from queue import Queue
+
+
+class CPU:
+    def __init__(self, inputqueue):
+        self.X = 1
+        self.queue = inputqueue
+
+    def cycle_accurate_X(self):
+        q = self.queue
+        while not q.empty():
+            instruction = q.get()
+            if len(instruction) == 1:
+                assert instruction[0] == "noop"
+                yield self.X
+            elif len(instruction) == 2:
+                assert instruction[0] == "addx"
+                yield self.X
+                yield self.X
+                self.X += int(instruction[1])
+
+
+def display_output_lines(signal_generator, width=40, height=6):
+    for y in range(height):
+        line = []
+        for x in range(width):
+            cpu_x = next(signal_generator)
+            sprite_position = range(cpu_x - 1, cpu_x + 2)
+            if x in sprite_position:
+                line.append("#")
+            else:
+                line.append(".")
+        yield line
+
+
+if __name__ == "__main__":
+    # from io import StringIO
+    # puzzle_input = StringIO("addx 15\naddx -11\naddx 6\naddx -3\naddx 5\naddx -1\naddx -8\naddx 13\naddx 4\nnoop\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx -35\naddx 1\naddx 24\naddx -19\naddx 1\naddx 16\naddx -11\nnoop\nnoop\naddx 21\naddx -15\nnoop\nnoop\naddx -3\naddx 9\naddx 1\naddx -3\naddx 8\naddx 1\naddx 5\nnoop\nnoop\nnoop\nnoop\nnoop\naddx -36\nnoop\naddx 1\naddx 7\nnoop\nnoop\nnoop\naddx 2\naddx 6\nnoop\nnoop\nnoop\nnoop\nnoop\naddx 1\nnoop\nnoop\naddx 7\naddx 1\nnoop\naddx -13\naddx 13\naddx 7\nnoop\naddx 1\naddx -33\nnoop\nnoop\nnoop\naddx 2\nnoop\nnoop\nnoop\naddx 8\nnoop\naddx -1\naddx 2\naddx 1\nnoop\naddx 17\naddx -9\naddx 1\naddx 1\naddx -3\naddx 11\nnoop\nnoop\naddx 1\nnoop\naddx 1\nnoop\nnoop\naddx -13\naddx -19\naddx 1\naddx 3\naddx 26\naddx -30\naddx 12\naddx -1\naddx 3\naddx 1\nnoop\nnoop\nnoop\naddx -9\naddx 18\naddx 1\naddx 2\nnoop\nnoop\naddx 9\nnoop\nnoop\nnoop\naddx -1\naddx 2\naddx -37\naddx 1\naddx 3\nnoop\naddx 15\naddx -21\naddx 22\naddx -6\naddx 1\nnoop\naddx 2\naddx 1\nnoop\naddx -10\nnoop\nnoop\naddx 20\naddx 1\naddx 2\naddx 2\naddx -6\naddx -11\nnoop\nnoop\nnoop\n")
+    puzzle_input = open("day10.txt", "r")
+    with puzzle_input as f:
+        instructions = [tuple(line.split(" ")) for line in f.read().splitlines()]
+
+    CPU_cacheline = Queue()
+    for instruction in instructions:
+        CPU_cacheline.put(instruction)
+
+    cpu = CPU(CPU_cacheline)
+    for line in display_output_lines(cpu.cycle_accurate_X()):
+        print(''.join(line))
